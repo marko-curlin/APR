acdd7dfd59b9d4926665bb797af8d2a4bc82e7a0
Marko Ćurlin <marko.curlin@fer.hr>
2019-10-23 17:11:20 +0200

    Add partial solution

---
+++ first_homework/solution.py
@@ -0,0 +1,68 @@
+from first_homework.matrix import Matrix
+
+
+def forward_substitution(matrix: Matrix, vector: Matrix) -> Matrix:
+    if not matrix.is_square() or vector.rows != matrix.rows:
+        raise ValueError('The vector must be of the same size as the quadrant matrix!')
+
+    # result = []
+    # sum_element = 0
+    #
+    # for i in range(matrix.rows):
+    #     for j in range(i-1):
+    #         sum_element = matrix[i][j] * result[j]
+    #     result.append(vector[i] - sum_element)
+    result = vector[0]
+
+    for i in range(matrix.rows-1):
+        for j in range(i, matrix.rows):
+            result[j] -= matrix[j][i] * result[i]
+
+    return Matrix([result])
+
+
+def backward_substitution(matrix: Matrix, vector: Matrix) -> Matrix:
+    if not matrix.is_square() or vector.rows != matrix.rows:
+        raise ValueError('The vector must be of the same size as the quadrant matrix!')
+
+    # result = []
+    # sum_element = 0
+    #
+    # for i in range(matrix.rows, step=-1):
+    #     for j in range(i - 1):
+    #         sum_element = matrix[i][j] * result[j]
+    #     result.append(vector[i] - sum_element)
+
+    result = vector[0]
+
+    for i in range(matrix.rows)[::-1]:
+        result[i] /= matrix[i][i]
+        for j in range(i):
+            result[j] -= matrix[j][i] * result[i]
+
+    return Matrix([result])
+
+
+def LU_decomposition(matrix: Matrix):
+    if not matrix.is_square():
+        raise ValueError('The matrix must be a square matrix!')
+
+    LU_matrix = matrix.matrix
+
+    for i in range(matrix.rows - 1):
+        for j in range(i+1, matrix.cols):
+            matrix[j][i] /= matrix[i][i]
+            for k in range(i+1, matrix.rows):
+                matrix[j][k] -= matrix[j][i] * matrix[i][k]
+
+    return LU_matrix
+
+
+def LUP_decomposition(matrix: Matrix):
+    if not matrix.is_square():
+        raise ValueError('The matrix must be a square matrix!')
+
+    p = Matrix.eye_matrix(matrix.rows)
+
+    for i in range(matrix.rows):
+        pass
