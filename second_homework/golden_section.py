6ef13b888ca511320b97c3ac55a43112a3f88daa
Marko Ćurlin <marko.curlin@fer.hr>
2019-11-16 17:55:14 +0100

    Print steps in golden section

--- second_homework/golden_section.py
+++ second_homework/golden_section.py
@@ -5,24 +5,26 @@ from typing import Tuple
 K = 0.5 * (sqrt(5) - 1)
 
 
-def find_function_min(function, a: float = None, b: float = None, start_point: float = None, e: float = 10**-6) -> float:
+def find_function_min(function, a: float = None, b: float = None, start_point: float = None,
+                      e: float = 10**-6, enable_output=False) -> float:
     if a is None or b is None:
         if start_point is None:
             raise ValueError("If either 'a' or 'b' are not provided, the 'start_point' MUST be provided!")
 
         a, b = find_unimodal_interval(1, start_point, function)
 
-    return sum(find_smallest_interval(function, a, b, e)) / 2
+    return sum(find_smallest_interval(function, a, b, e, enable_output)) / 2
 
 
-def find_smallest_interval(function, a: float, b: float, e: float) -> Tuple[float, float]:
+def find_smallest_interval(function, a: float, b: float, e: float, enable_output=False) -> Tuple[float, float]:
     c = b - K * (b - a)
     d = a + K * (b - a)
 
     fc = function(c)
     fd = function(d)
 
-    print_values(function, a, b, c, d)
+    if enable_output:
+        print_values(function, a, b, c, d)
 
     while b - a > e:
         if fc < fd:
@@ -33,7 +35,8 @@ def find_smallest_interval(function, a: float, b: float, e: float) -> Tuple[floa
             fd = fc
             fc = function(c)
 
-            print_values(function, a, b, c, d)
+            if enable_output:
+                print_values(function, a, b, c, d)
         else:
             a = c
             c = d
@@ -42,7 +45,8 @@ def find_smallest_interval(function, a: float, b: float, e: float) -> Tuple[floa
             fc = fd
             fd = function(d)
 
-            print_values(function, a, b, c, d)
+            if enable_output:
+                print_values(function, a, b, c, d)
 
     return a, b
 
@@ -58,3 +62,5 @@ def print_values(function, a, b, c, d):
 
 if __name__ == '__main__':
     print(find_function_min(lambda x: (x - 4)**2, a=-2, b=6, start_point=0))
+
+    find_function_min(lambda x, y: (x + y)**2, a=[-2, -2], b=[6, 6], start_point=0)
