ea0fb0c1eb3ec0d350a811ac5af511d0b0025055
Marko Ćurlin <marko.curlin@fer.hr>
2019-11-17 12:53:08 +0100

    Enable output of intermediate values in hooke-jeeves

--- second_homework/hooke_jeeves.py
+++ second_homework/hooke_jeeves.py
@@ -3,12 +3,16 @@ from copy import deepcopy
 from operator import add, sub
 
 
-def find_function_min(function, start_point: List[float], delta_x: float, epsilon: float = 10**-6) -> List[float]:
+def find_function_min(function, start_point: List[float], delta_x: float,
+                      epsilon: float = 10**-6, enable_output = False) -> List[float]:
     Xp = deepcopy(start_point)
     Xb = deepcopy(start_point)
 
     while True:
         Xn = explore(function, Xp, delta_x)
+
+        if enable_output:
+            print_values(function, Xb, Xp, Xn)
 
         if function(*Xn) < function(*Xb):
             Xp = subtract_list_elements(multiply_list_elements(Xn, 2), Xb)
@@ -55,5 +59,15 @@ def multiply_list_elements(_list, mul):
     return [mul * x for x in _list]
 
 
+def print_values(function, base_point, explore_point, new_point):
+    value_Xb = function(*base_point)
+    value_Xp = function(*explore_point)
+    value_Xn = function(*new_point)
+
+    print("Xb = {}, f(Xb) = {} ... Xp = {}, f(Xp) = {} ... Xn = {}, f(Xn) = {}".format(base_point, value_Xb,
+                                                                                       explore_point, value_Xp,
+                                                                                       new_point, value_Xn))
+
+
 if __name__ == '__main__':
-    print(find_function_min(lambda x, y: x**2 + 4 * y**2, [7, 3], 1, 0.1))
+    print(find_function_min(lambda x, y: x**2 + 4 * y**2, [7, 3], 1, 0.1, enable_output=True))
