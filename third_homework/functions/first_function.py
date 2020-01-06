b58c037e6d41c9761459f59709d61f2e7439f726
Marko Ćurlin <marko.curlin@fer.hr>
2020-01-06 20:02:45 +0100

    Fix mistake in first function

--- third_homework/functions/first_function.py
+++ third_homework/functions/first_function.py
@@ -3,7 +3,7 @@ from .function import Function
 
 class FirstFunction(Function):
     def __init__(self):
-        super().__init__(lambda x, y: 100 * (y - x**2) + (1 - x)**2)
+        super().__init__(lambda x, y: 100 * (y - x**2)**2 + (1 - x)**2)
 
     def _calculate_gradient(self, *x):
         return [400 * x[0] * (x[0]**2 - x[1]) + 2 * x[0] - 2, -200 * x[0]**2 + 200 * x[1]]
