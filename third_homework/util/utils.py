4328c77184bd46ee5f16e9f6c330f927dfdc5cb9
Marko Ćurlin <marko.curlin@fer.hr>
2020-01-06 22:02:51 +0100

    Remove float from type hint

--- third_homework/util/utils.py
+++ third_homework/util/utils.py
@@ -6,13 +6,13 @@ def norm(vector: List) -> float:
     return sqrt(sum([x**2 for x in vector]))
 
 
-def multiply_each_element(vector: List[float], x: float) -> List[float]:
+def multiply_each_element(vector: List, x: float) -> List:
     return [x * element for element in vector]
 
 
-def add_elements_on_same_index(vector1: List[float], vector2: List[float]) -> List[float]:
+def add_elements_on_same_index(vector1: List, vector2: List) -> List:
     return [el1 + el2 for el1, el2 in zip(vector1, vector2)]
 
 
-def sub_elements_on_same_index(vector1: List[float], vector2: List[float]) -> List[float]:
+def sub_elements_on_same_index(vector1: List, vector2: List) -> List:
     return [el1 - el2 for el1, el2 in zip(vector1, vector2)]
