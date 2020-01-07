274fda5a3edc0d7ce6507521ef8cc3f0c4c1cf05
Marko Ćurlin <marko.curlin@fer.hr>
2020-01-08 00:59:20 +0100

    Add another util function, add float type to List hints

--- third_homework/util/utils.py
+++ third_homework/util/utils.py
@@ -2,17 +2,21 @@ from typing import List
 from math import sqrt
 
 
-def norm(vector: List) -> float:
+def norm(vector: List[float]) -> float:
     return sqrt(sum([x**2 for x in vector]))
 
 
-def multiply_each_element(vector: List, x: float) -> List:
+def multiply_each_element(vector: List[float], x: float) -> List[float]:
     return [x * element for element in vector]
 
 
-def add_elements_on_same_index(vector1: List, vector2: List) -> List:
+def add_elements_on_same_index(vector1: List[float], vector2: List[float]) -> List[float]:
     return [el1 + el2 for el1, el2 in zip(vector1, vector2)]
 
 
-def sub_elements_on_same_index(vector1: List, vector2: List) -> List:
+def sub_elements_on_same_index(vector1: List[float], vector2: List[float]) -> List[float]:
     return [el1 - el2 for el1, el2 in zip(vector1, vector2)]
+
+
+def mul_elements_on_same_index(vector1: List[float], vector2: List[float]) -> List[float]:
+    return [el1 * el2 for el1, el2 in zip(vector1, vector2)]
