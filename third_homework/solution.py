f0d2995d79e4d3496f3776ee442b5770ceb8111b
Marko Ćurlin <marko.curlin@fer.hr>
2020-01-08 00:59:34 +0100

    Add third task to solution

--- third_homework/solution.py
+++ third_homework/solution.py
@@ -1,18 +1,25 @@
 from prettytable import PrettyTable
 
 from third_homework.algorithms.gradient import find_function_min as gradient
-from third_homework.algorithms.newthon_raphson import find_function_min as newthon_raphson
+from third_homework.algorithms.newthon_raphson import find_function_min as newton_raphson
+from third_homework.algorithms.box import find_function_min as box
+
 from third_homework.functions.function_factory import get_function_and_start_point
+
+from third_homework.limits.implicit_limits import InequationLimit
+from third_homework.limits.explicit_limits import LowerLimit, UpperLimit
 
 PRINT_STEPS = False
 
 
 def main():
-    print_headers('FIRST TASK', '#'*70)
-    first_task()
-    first_task_golden()
-    print_headers('SECOND TASK', '#' * 70)
-    second_task()
+    # print_headers('FIRST TASK', '#'*70)
+    # first_task()
+    # first_task_golden()
+    # print_headers('SECOND TASK', '#' * 70)
+    # second_task()
+    print_headers('THIRD TASK', '#' * 70)
+    third_task()
 
 
 def print_headers(title, marker):
@@ -51,12 +58,31 @@ def second_task():
 
     print_headers('First function', '-'*10)
     func, start_point = get_function_and_start_point(1)
-    result = newthon_raphson(func, start_point, shift_type='golden', enable_output=PRINT_STEPS)
+    result = newton_raphson(func, start_point, shift_type='golden', enable_output=PRINT_STEPS)
     print_result(result, func)
 
     print_headers('Second function', '-'*10)
     func, start_point = get_function_and_start_point(2)
-    result = newthon_raphson(func, start_point, shift_type='golden', enable_output=PRINT_STEPS)
+    result = newton_raphson(func, start_point, shift_type='golden', enable_output=PRINT_STEPS)
+    print_result(result, func)
+
+
+def third_task():
+    implicit_limit1 = InequationLimit(lambda x, y: y - x)
+    implicit_limit2 = InequationLimit(lambda x, y: 2 - x)
+    explicit_limit1 = LowerLimit([-100, -100])
+    explicit_limit2 = UpperLimit([100, 100])
+
+    print_headers('BOX', '$' * 20)
+
+    print_headers('First function', '-' * 10)
+    func, start_point = get_function_and_start_point(1)
+    result = box(func, start_point, [implicit_limit1, implicit_limit2], [explicit_limit1, explicit_limit2])
+    print_result(result, func)
+
+    print_headers('Second function', '-' * 10)
+    func, start_point = get_function_and_start_point(2)
+    result = box(func, start_point, [implicit_limit1, implicit_limit2], [explicit_limit1, explicit_limit2])
     print_result(result, func)
 
 
