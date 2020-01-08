f7bef874fe944799ce07f1e58d2b3b12ba190bb0
Marko Ćurlin <marko.curlin@fer.hr>
2020-01-08 16:24:00 +0100

    Add step-by-step print to transformation algorithm

--- third_homework/algorithms/constrained_optimization_transforamtion.py
+++ third_homework/algorithms/constrained_optimization_transforamtion.py
@@ -1,5 +1,7 @@
 from copy import deepcopy
 
+from prettytable import PrettyTable
+
 from third_homework.functions.function import Function, InnerPointFunction, TransformedFunction
 from third_homework.util.utils import *
 from third_homework.util.simplex_method import find_function_min as simplex_min
@@ -7,6 +9,7 @@ from third_homework.util.simplex_method import find_function_min as simplex_min
 
 def find_function_min(function: Function, start_point: List[float], implicit_limits: List[ImplicitLimit] = None,
                       t: float = 1, e: float = 10**-6, enable_output=False) -> List[float]:
+    table = PrettyTable(['total_iterations', 'previous_point', 'current_point', 'min_value', 'break_condition_value'])
 
     if is_point_within_limits(start_point, implicit_limits=implicit_limits):
         current_point = list(start_point)
@@ -18,27 +21,33 @@ def find_function_min(function: Function, start_point: List[float], implicit_lim
 
     total_iterations, iterations_without_improvement = 1, 0
     while True:
-        point_before_shift = deepcopy(current_point)
+        previous_point = deepcopy(current_point)
 
         current_point = simplex_min(function, current_point)
 
-        if max_diff_on_same_indices(point_before_shift, current_point) < e:
+        if max_diff_on_same_indices(previous_point, current_point) < e:
             break
 
         current_value = function(*current_point)
+
+        table.add_row([total_iterations+1, previous_point, current_point, minimum_value, max_diff_on_same_indices(previous_point, current_point)])
+
         if current_value < minimum_value:
             minimum_value = current_value
             iterations_without_improvement = 0
         else:
             iterations_without_improvement += 1
             if iterations_without_improvement == MAX_ITERATIONS:
-                # if enable_output:
-                #     print(output)
+                if enable_output:
+                    print(table)
                 raise ArithmeticError('It exceeded maximum nr of iterations')
 
         total_iterations += 1
 
         function.t *= 10
+
+    if enable_output:
+        print(table)
 
     return current_point
 
