727c96268eac6f1fc2c4d7469fb8c082d965f421
Marko Ćurlin <marko.curlin@fer.hr>
2020-01-11 13:03:16 +0100

    Add utils file

---
+++ fourth_homework/utils.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+def add_elements_on_same_index(vector1: List[float], vector2: List[float]) -> List[float]:
+    return [el1 + el2 for el1, el2 in zip(vector1, vector2)]
+
+
+def set_within_limits(vector, lower_limit, upper_limit):
+    new_vector = []
+
+    for el in vector:
+        if el < lower_limit:
+            new_vector.append(lower_limit)
+        elif el > upper_limit:
+            new_vector.append(upper_limit)
+        else:
+            new_vector.append(el)
+
+    return new_vector
