08bc4eb07dc9b06a3af05d67c960cf1e1ca1845c
Marko Ćurlin <marko.curlin@fer.hr>
2020-01-13 00:18:32 +0100

    Make binary uniform crossover applicable to unlimited units

--- fourth_homework/crossovers/crossover.py
+++ fourth_homework/crossovers/crossover.py
@@ -52,7 +52,7 @@ class BinarySingleCrossoverPoint:
 
 class BinaryUniformCrossover:
     @staticmethod
-    def create_new_unit(unit1: BinaryUnit, unit2: BinaryUnit):
+    def create_new_unit1(unit1: BinaryUnit, unit2: BinaryUnit):
         new_point: List[List[int]] = []
 
         for i in range(unit1.dimension):
@@ -64,3 +64,31 @@ class BinaryUniformCrossover:
             new_point.append(new_coordinate)
 
         return BinaryUnit(new_point, unit1.lower_limit, unit1.upper_limit)
+
+    @staticmethod
+    def create_new_unit(*units: Tuple[BinaryUnit]):
+        new_point: List[List[int]] = []
+
+        for i in range(units[0].dimension):
+            new_coordinate = []
+
+            for j in range(units[0].nr_of_bits):
+                new_coordinate.append(choose_single_byte_from_units(i, j, units))
+
+            new_point.append(new_coordinate)
+
+        return BinaryUnit(new_point, units[0].lower_limit, units[0].upper_limit)
+
+
+def choose_single_byte_from_units(dimension, bit, units):
+    prob_for_units = []
+    for i in range(len(units) + 1):
+        prob_for_units.append(i / len(units))
+
+    random_value = random()
+
+    for i in range(len(prob_for_units) - 1):
+        if prob_for_units[i] <= random_value < prob_for_units[i+1]:
+            return units[i].point[dimension][bit]
+
+    raise LookupError('Something went wrong.')
