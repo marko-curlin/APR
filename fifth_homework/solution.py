f784f5f2624e1d45d8a4ddd858596dd9ea9c97ec
Marko Ćurlin <marko.curlin@fer.hr>
2020-01-18 21:43:15 +0100

    Create solution.py

---
+++ fifth_homework/solution.py
@@ -0,0 +1,29 @@
+def main():
+    print_headers('FIRST TASK', '#'*70)
+    first_task()
+    # print_headers('SECOND TASK', '#'*70)
+    # second_task_float()
+    # second_task_binary()
+    # print_headers('THIRD TASK', '#'*70)
+    # third_task()
+    # print_headers('FOURTH TASK', '#'*70)
+    # fourth_task()
+    # print_headers('FIFTH TASK', '#'*70)
+    # fifth_task()
+
+
+def first_task():
+    A = [[0, 1], [-1, 0]]
+    x0 = [1, 1]
+    T = 0.01
+    t_max = 10
+
+    # start solving
+
+
+def print_headers(title, marker):
+    print("{} {} {}".format(marker, title, marker))
+
+
+if __name__ == '__main__':
+    main()
